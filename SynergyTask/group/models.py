1551037d2fae736fcf87bba37bd004fd6128c2ea
Orest <Borovyorest@gmail.com>
2019-02-18 14:10:01 +0200

    Create model for group
    Create model with fields "name", "description"
    Add methods "__str__", "to_dict", "create", "update"

--- SynergyTask/group/models.py
+++ SynergyTask/group/models.py
@@ -1,3 +1,50 @@
-from django.db import models
+"""This module implements class that represents the group entity."""
 
-# Create your models here.
+from django.db import models, OperationalError, transaction
+
+
+class Group(models.Model):
+    """Model for Group entity."""
+    name = models.CharField()
+    description = models.CharField(default='', blank=True)
+
+    def __str__(self):
+        """Method that returns route instance as string."""
+        return f'Group with id {self.id} and name {self.name}'
+
+    def to_dict(self):
+        """Method that returns dict with object's attributes."""
+        return {
+            'group_id': self.id,
+            'name': self.name,
+            'description': self.description
+        }
+
+    @classmethod
+    def create(cls, name, description=None):
+        """Method for object creation."""
+        group = cls()
+        group.description = description if description is not None else ''
+
+        try:
+            group.name = name
+            group.save()
+            return group
+        except (ValueError, OperationalError):
+            return None
+
+    def update(self, name, description):
+        """Update object parameters."""
+        with transaction.atomic():
+            if name:
+                self.name = name
+
+            if description:
+                self.description = description
+
+            try:
+                self.save()
+            except (ValueError, OperationalError):
+                return False
+
+        return True
