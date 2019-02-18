9d991fea57dfdcf2cb257f9bca9e35b8fab495ce
Orest <Borovyorest@gmail.com>
2019-02-18 14:13:02 +0200

    Create model for custom user
    Create model with fields "nickname", "created", "group"
    Add methods "__str__", "to_dict", "create", "update"

--- SynergyTask/custom_user/models.py
+++ SynergyTask/custom_user/models.py
@@ -1,3 +1,53 @@
-from django.db import models
+"""This module implements class that represents the custom user entity."""
 
-# Create your models here.
+from django.db import models, IntegrityError, OperationalError, transaction
+
+from group.models import Group
+
+
+class CustomUser(models.Model):
+    """Model for CustomUser entity."""
+    nickname = models.CharField()
+    created = models.DateTimeField(auto_now_add=True)
+    group = models.ForeignKey(Group, on_delete=models.PROTECT)
+
+    def __str__(self):
+        """Method that returns route instance as string."""
+        return f'User with id {self.id} and nickname {self.nickname}'
+
+    def to_dict(self):
+        """Method that returns dict with object's attributes."""
+        return {
+            'user_id': self.id,
+            'nickname': self.nickname,
+            'group_id': self.group.id
+        }
+
+    @classmethod
+    def create(cls, nickname, group=None):
+        """Method for object creation."""
+        user = cls()
+
+        try:
+            user.nickname = nickname
+            user.group = group
+            user.save()
+            return user
+        except (ValueError, IntegrityError, OperationalError):
+            return None
+
+    def update(self, nickname, group):
+        """Update object parameters."""
+        with transaction.atomic():
+            if nickname:
+                self.nickname = nickname
+
+            if group:
+                self.group = group
+
+            try:
+                self.save()
+            except (ValueError, IntegrityError, OperationalError):
+                return False
+
+        return True
